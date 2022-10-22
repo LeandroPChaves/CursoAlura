5d5df988a13266391bc0a0ebc70dd991d946c83f
Leandro Chaves <leandropadilhachaves@gmail.com>
2022-10-22 09:20:26 -0300

    Incluindo mais códigos da matéria objetos

--- javaScript_/objetos/lista-unica.js
+++ javaScript_/objetos/lista-unica.js
@@ -1,6 +1,7 @@
 //Extrair de uma listagem de clientes apenas as informações de dependentes e montar uma lista única, para análise de outros departamentos do banco.
 
-const cliente = {
+const clientes = [{
+  
     nome: "André",
     cpf: "12543652266",
     dependentes: [{
@@ -13,7 +14,9 @@ const cliente = {
         parentesco: "filha", 
         dataNasc: "04/01/2014"
     }],
+},
 
+ {
     nome: "Juliana", 
     cpf: "56767867867",
     dependentes: [{
@@ -21,6 +24,15 @@ const cliente = {
         parentesco: "filha",
         dataNasc: "30/08/2020"
     }],
-}
+  
+}]
+
+const listaDependentes = [...clientes[0].dependentes,...clientes[1].dependentes]
+
+console.table (listaDependentes);
+
+//(...) spread operator, ou sintaxe de espalhamento. Este operador copia as propriedades de objetos para outros, “espalhando” os conteúdos. 
+
+
 
 
